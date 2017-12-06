b81b450f4b324aa199b0cd76b6c9c5e1d1624989
Lukasz Chmielewski <lu.chmielewski@gmail.com>
2017-12-06 06:41:51 +0100

    Day 6 both parts

---
+++ Puzzles/Day06/PuzzlePartOne.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Puzzles\Day06;
+
+class PuzzlePartOne extends Puzzle
+{
+    private $sum = 1;
+
+    public function processInput()
+    {
+        $blocks = preg_split('/\s+/', $this->input[0]);
+
+        $knownBlocks = [
+            join(" ", $blocks)
+        ];
+
+        while (1) {
+            $maxValue = $blocks[0];
+            $maxIndex = 0;
+            for ($i = 1; $i < count($blocks); $i++) {
+                if ($blocks[$i] > $maxValue) {
+                    $maxValue = $blocks[$i];
+                    $maxIndex = $i;
+                }
+            }
+
+            $blocks[$maxIndex] = 0;
+
+            for ($j = 1; $j <= $maxValue; $j++) {
+                $insertIndex = ($maxIndex + $j) % count($blocks);
+                $blocks[$insertIndex]++;
+            }
+            $afterBlocks = $blocks;
+            //var_dump($knownBlocks);
+            if (in_array(join(" ", $afterBlocks), $knownBlocks)) {
+                break;
+            } else {
+                $this->sum++;
+                $knownBlocks[] = join(" ", $afterBlocks);
+            }
+        }
+
+    }
+
+    public function renderSolution()
+    {
+        echo 'Solution: ' . $this->sum . PHP_EOL;
+    }
+}
